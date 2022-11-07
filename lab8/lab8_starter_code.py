c0a7b742f8d4e3fe3c6e3561fdf8cc848082e240
sydneykimbell <81785228+sydneykimbell@users.noreply.github.com>
2022-11-07 14:00:02 -0500

    Add files via upload

---
+++ lab8/lab8_starter_code.py
@@ -0,0 +1,23 @@
+# program 1:
+
+# hard code the states and capitals lists, referencing the data from capitals.txt
+states = []
+capitals = []
+
+# ask user to enter a state, then print out the capital of that state
+# you can create a linear search function or use index() to get the index of the state, then print the capital at that index
+
+
+# program 2:
+
+# first, read in the file
+with open('filename', 'r') as file:
+    pass # split the file by line here
+
+# then use the same code from program 1
+
+# program 3:
+
+# read in the file, same as program 2
+# then create a game to quiz the user on states and capitals
+
